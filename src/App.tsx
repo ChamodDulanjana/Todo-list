e27fb04ac13886bbeab2b70584ac64269b8d454c
ChamodDulanjana <chamodperera128@gmail.com>
2024-08-08 00:39:22 +0530

    Commited

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+   <main className="flex flex-col items-center justify-center h-full">
+       <div className="flex flex-col items-center justify-center w-full h-full bg-[#6c5ce7] z-20">
+           <h1 className="m-5 font-medium text-3xl font-Neucha text-amber-50">Todo-List</h1>
+       </div>
+       <div className="flex flex-col items-center justify-center h-full">
+           jhjhjhj
+       </div>
+
+   </main>
+  )
+}
+
+export default App
